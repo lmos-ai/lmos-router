81faaace83276223a2606ffb437b61287ec50f26
bharatnpti <bharatnpti@gmail.com>
2025-01-12 20:54:04 +0530

    fix: #42 change scope of libraries

--- lmos-router-llm/build.gradle.kts
+++ lmos-router-llm/build.gradle.kts
@@ -6,13 +6,8 @@ dependencies {
     api(project(":lmos-router-core"))
     implementation("org.slf4j:slf4j-api:1.7.25")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.7.3")
-    compileOnly("dev.langchain4j:langchain4j-open-ai:0.36.2")
-    compileOnly("dev.langchain4j:langchain4j-anthropic:0.36.2")
-    compileOnly("dev.langchain4j:langchain4j-google-ai-gemini:0.36.2")
-    compileOnly("dev.langchain4j:langchain4j-ollama:0.36.2")
-
-    testImplementation("dev.langchain4j:langchain4j-open-ai:0.36.2")
-    testImplementation("dev.langchain4j:langchain4j-anthropic:0.36.2")
-    testImplementation("dev.langchain4j:langchain4j-google-ai-gemini:0.36.2")
-    testImplementation("dev.langchain4j:langchain4j-ollama:0.36.2")
+    implementation("dev.langchain4j:langchain4j-open-ai:0.36.2")
+    implementation("dev.langchain4j:langchain4j-anthropic:0.36.2")
+    implementation("dev.langchain4j:langchain4j-google-ai-gemini:0.36.2")
+    implementation("dev.langchain4j:langchain4j-ollama:0.36.2")
 }
