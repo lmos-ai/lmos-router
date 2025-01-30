4b088a271601d600d9cade0dfb3aacf4318ca07f
Henning Sudbrock <henning.sudbrock@telekom.de>
2025-01-30 15:16:33 +0100

    Use latest milestone release for spring-ai, to have a more stable build
    With the snapshot dependencies used before, the build broke due to compilation issues; with the latest milestone the compilation works again; we might see it breaking when upgrading to the next milestone release, but than at least we know it is due to the upgrade of the dependency.

--- lmos-router-llm-spring-boot-starter/build.gradle.kts
+++ lmos-router-llm-spring-boot-starter/build.gradle.kts
@@ -9,9 +9,9 @@ dependencies {
     api(project(":lmos-router-llm"))
     implementation("org.springframework.boot:spring-boot-autoconfigure:$springBootVersion")
     implementation("org.springframework.boot:spring-boot-configuration-processor:$springBootVersion")
-    implementation("org.springframework.ai:spring-ai-core:1.0.0-SNAPSHOT")
+    implementation("org.springframework.ai:spring-ai-core:1.0.0-M5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.7.3")
-    testImplementation("org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-SNAPSHOT")
+    testImplementation("org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-M5")
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
 }
