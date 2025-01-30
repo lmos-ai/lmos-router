lmos-ai/lmos-router

commit 7998e3e371cec06c09d9279777b11ce8ae9da211

Add support for chat models from Azure-OpenAI (#48)
* Add support for chat models from Azure-OpenAI

Currently, the router supports openai chat models, but not the openai chat models provided by azure.

* Add a flow test for the azure openai model

lmos-router-llm/src/main/kotlin/org/eclipse/lmos/router/llm/LangChainModelClient.kt
@@ -6,6 +6,7 @@ package org.eclipse.lmos.router.llm
 
 import dev.langchain4j.data.message.AiMessage
 import dev.langchain4j.model.anthropic.AnthropicChatModel
+import dev.langchain4j.model.azure.AzureOpenAiChatModel
 import dev.langchain4j.model.chat.ChatLanguageModel
 import dev.langchain4j.model.chat.request.ResponseFormat
 import dev.langchain4j.model.chat.request.ResponseFormatType
@@ -85,6 +86,22 @@ class LangChainChatModelFactory private constructor() {
                     model.build()
                 }
 
+                LangChainClientProvider.AZURE_OPENAI.name.lowercase() -> {
+                    require(properties.baseUrl != null) { "baseUrl is required for '${LangChainClientProvider.AZURE_OPENAI.name.lowercase()}' provider" }
+
+                    val model =
+                        AzureOpenAiChatModel.builder()
+                            .endpoint(properties.baseUrl)
+                            .apiKey(properties.apiKey)
+                            .deploymentName(properties.model)
+                            .maxTokens(properties.maxTokens)
+                            .temperature(properties.temperature)
+
+                    properties.topP?.let { model.topP(it) }
+
+                    model.build()
+                }
+
                 LangChainClientProvider.GEMINI.name.lowercase() -> {
                     val model =
                         GoogleAiGeminiChatModel.builder()
@@ -142,6 +159,7 @@ class LangChainChatModelFactory private constructor() {
 enum class LangChainClientProvider {
     OPENAI,
     ANTHROPIC,
+    AZURE_OPENAI,
     GEMINI,
     OLLAMA,
     OTHER,

lmos-router-llm/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.7.3")
     implementation("dev.langchain4j:langchain4j-open-ai:0.36.2")
     implementation("dev.langchain4j:langchain4j-anthropic:0.36.2")
+    implementation("dev.langchain4j:langchain4j-azure-open-ai:0.36.2")
     implementation("dev.langchain4j:langchain4j-google-ai-gemini:0.36.2")
     implementation("dev.langchain4j:langchain4j-ollama:0.36.2")
 }

lmos-router-llm/src/test/kotlin/SampleLLMFlow.kt
@@ -144,6 +144,46 @@ class SampleLLMFlow {
     }
 
     @Test
+    fun `sample test with external prompt agent spec in json format with azure openai model`() {
+        require(System.getenv("AZURE_OPENAI_API_KEY") != null) {
+            "Please set the AZURE_OPENAI_API_KEY environment variable to run the tests"
+        }
+
+        require(System.getenv("AZURE_OPENAI_BASE_URL") != null) {
+            "Please set the AZURE_OPENAI_BASE_URL environment variable to run the tests"
+        }
+
+        require(System.getenv("AZURE_OPENAI_MODEL") != null) {
+            "Please set the AZURE_OPENAI_MODEL environment variable to run the tests"
+        }
+
+        val routingSpecsResolver =
+            LLMAgentRoutingSpecsResolver(
+                JsonAgentRoutingSpecsProvider(jsonFilePath = "src/test/resources/agentRoutingSpecs.json"),
+                ExternalModelPromptProvider(
+                    "src/test/resources/prompt_agentRoutingSpec_json.txt",
+                    AgentRoutingSpecListType.JSON,
+                ),
+                modelClient =
+                    LangChainModelClient(
+                        LangChainChatModelFactory.createClient(
+                            ModelClientProperties(
+                                provider = "azure_openai",
+                                apiKey = System.getenv("AZURE_OPENAI_API_KEY"),
+                                baseUrl = System.getenv("AZURE_OPENAI_BASE_URL"),
+                                model = System.getenv("AZURE_OPENAI_MODEL"),
+                            ),
+                        ),
+                    ),
+            )
+        val context = Context(listOf(AssistantMessage("Hello")))
+        val input = UserMessage("Can you help me find a new phone?")
+        val result = routingSpecsResolver.resolve(context, input)
+        assert(result is Success)
+        assert((result as Success).getOrNull()?.name == "offer-agent")
+    }
+
+    @Test
     fun `sample test with external prompt agent spec in json format with ollama model`() {
         val routingSpecsResolver =
             LLMAgentRoutingSpecsResolver(

commit 6779ff6609c7d0e170598a83e6df246d92f507a9

Fix ktlint issues that were introduced with 7998e3e371cec06c09d9279777b11ce8ae9da211

lmos-router-llm/src/main/kotlin/org/eclipse/lmos/router/llm/LangChainModelClient.kt
@@ -14,6 +14,7 @@ import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel
 import dev.langchain4j.model.ollama.OllamaChatModel
 import dev.langchain4j.model.openai.OpenAiChatModel.OpenAiChatModelBuilder
 import org.eclipse.lmos.router.core.*
+import org.eclipse.lmos.router.llm.LangChainClientProvider.AZURE_OPENAI
 
 /**
  * A model client that uses Langchain4j(https://docs.langchain4j.dev/) to call a language model.
@@ -86,8 +87,8 @@ class LangChainChatModelFactory private constructor() {
                     model.build()
                 }
 
-                LangChainClientProvider.AZURE_OPENAI.name.lowercase() -> {
-                    require(properties.baseUrl != null) { "baseUrl is required for '${LangChainClientProvider.AZURE_OPENAI.name.lowercase()}' provider" }
+                AZURE_OPENAI.name.lowercase() -> {
+                    require(properties.baseUrl != null) { "baseUrl is required for '${AZURE_OPENAI.name.lowercase()}' provider" }
 
                     val model =
                         AzureOpenAiChatModel.builder()